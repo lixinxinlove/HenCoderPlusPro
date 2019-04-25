a12abb5bdff0f4b1ae2a3f92017021f43379533b
lixinxin <895330766@qq.com>
2019-04-25 16:05:16 +0800

    Initial commit

---
+++ app/src/main/java/com/lixinxinlove/hencoderpluspro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lixinxinlove.hencoderpluspro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
